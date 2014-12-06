375a3a82976868cfa93286fe81fe37af2e4d59bb
squirelo <valentin.squirelo@gmail.com>
2014-12-06 15:51:05 +0100

    update ardu script

--- arduitest/arduitest.ino
+++ arduitest/arduitest.ino
@@ -1,22 +1,38 @@
-/*
+  /*
  ardui turret test
  */
 
 
 int flywheel = 12;
-int trigger= 13
+int trigger= 13;
+
+char buff[1];
 
 void setup() {
   pinMode(flywheel, OUTPUT);
   pinMode(trigger, OUTPUT);
+  digitalWrite(flywheel, HIGH);
+  digitalWrite(trigger, HIGH);
+  
+  Serial.begin(9600);
+  Serial.setTimeout(424242);
+  
+  digitalWrite(flywheel, LOW);
 }
 
 void loop() {
+  /*digitalWrite(flywheel, LOW);
+  delay(2000);
+  digitalWrite(trigger, LOW);  
+  delay(500);
+  digitalWrite(trigger, HIGH);
+  delay(500);
   digitalWrite(flywheel, HIGH);
-  delay(500);               
-  digitalWrite(trigger, HIGH);  
-  delay(300);
-  digitalWrite(flywheel, LOW);
+  
+  delay(6000);*/
+ 
+  Serial.readBytes(buff, sizeof(buff));
   digitalWrite(trigger, LOW);
-  delay(10000);  
+  delay(150);
+  digitalWrite(trigger, HIGH);
 }
